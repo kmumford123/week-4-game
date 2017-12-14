681d13ddfea784abe94907c140189f50b601cbc5
Khamel Mumford <khamel.mumford@gmail.com>
2017-12-13 22:13:55 -0600

    Latest Save

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,28 @@
 // Generate a random number
-var randomNumber = Math.floor(Math.random()*50);
+var randomNumber = Math.floor(Math.random() *50+30);
+console.log(randomNumber);
+var buttonNumber = Math.floor(Math.random () *12+2);
 
 //  Set up four crystals that generate random values
-var buttonNumber = Math.floor(Math.random () * 12);
-$(".#symbol-buttons").on("click", function () {
-    $("#button-value").html(`<p>${buttonNumber}<p>`);
+$("#thatbutton").on("click", function () {
+    console.log(buttonNumber);
+    var firstNumber = buttonNumber;
+    var currentNumber = firstNumber + buttonNumber;
+    console.log(firstNumber);
+    console.log(currentNumber);
+    $("#current-number").html(`${currentNumber}`);
 });
+
+//Set up Wins and Losses
+
+if (currentNumber === randomNumber) {
+    alert(`Nice Job you equaled ${randomNumber}.  You win!!!`);
+    
+}
+else if (currentNumber > randomNumber) {
+    alert(`Sorry.  You went over ${randomNmber}.  You Lose :-(`);
+}
+
+else {
+    ????
+}
