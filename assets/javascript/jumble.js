b243c0658331605e0cc6c2c98b440ba0e283a88b
Khamel Mumford <khamel.mumford@gmail.com>
2017-12-14 18:26:15 -0600

    Latest Save

---
+++ assets/javascript/jumble.js
@@ -0,0 +1,26 @@
+// var randomNumber = Math.floor(Math.random() *50) +25;
+//  Set up four crystals that generate random values
+// $("#ButtonItUp").on("click", function () {
+//     var clickNumber = buttonNumber + 2;
+//     currentScore = currentScore + clickNumber;
+//     console.log(clickNumber);
+//     $("#current-number").text(`Your Current Score is ${currentScore}`);
+//     winCheck();
+// });
+//These are possible ways to
+
+// if (currentScore == randomNumber) {
+//     alert(`Nice Job you equaled ${randomNumber}.  You win!!!`);
+//     wins = wins+1;
+//     $("#wins").text(`Wins: ${wins}`);
+//     gameReset();
+    
+// }
+// else if (currentScore > randomNumber) {
+//     alert(`Sorry.  You went over ${randomNumber}.  You Lose :-(`);
+//     losses = losses+1;
+//     $("#losses").text(`Losses: ${losses}`);
+//     gameReset();
+//     console.log("you went over");
+// }
+// });
